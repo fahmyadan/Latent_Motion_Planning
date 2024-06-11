f7f147aae56ce2e7f57b59c6847a9f075192dec1
fahmyadan <fahmy.adan14@imperial.ac.uk>
2024-06-11 15:45:44 +0100

    fix environment dependency issues

--- setup.bash
+++ setup.bash
@@ -4,15 +4,18 @@ eval "$(conda shell.bash hook)"
 
 conda create -n z_plan python=3.10 -y
 
+eval "$(conda shell.bash hook)"
 conda activate z_plan 
 
 pip install poetry
+
+pip install -U --force-reinstall charset-normalizer chardet
 
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
 
 poetry install 
 
-cd latent_motion_planning/envs/HighwayEnv || exit
+cd latent_motion_planning/envs/HighwayEnv  && git checkout master || exit
 
 pip install -e . 
 
